869119deb9a923261b1de645588e31ef6e2375ef
Teo Cic <acmg04@gmail.com>
2026-05-06 05:04:37 -0400

    Add proof: UpdatedPrTest

---
+++ lean/ProofCollection/Submissions/UpdatedPrTest.lean
@@ -0,0 +1,8 @@
+import ProofCollection.Basic
+
+namespace ProofCollection.Submissions
+
+theorem auto_index_test (n : Nat) : 0 + n = n := by
+  exact Nat.zero_add n
+
+end ProofCollection.Submissions

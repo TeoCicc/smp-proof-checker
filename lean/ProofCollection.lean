7fe9769c59728d3734e2f4c062f83b9eff207965
Teo Cic <acmg04@gmail.com>
2026-05-06 15:11:04 -0400

    Import FrontendTest into ProofCollection

--- lean/ProofCollection.lean
+++ lean/ProofCollection.lean
@@ -2,3 +2,4 @@ import ProofCollection.Basic
 import ProofCollection.Submissions.Example
 import ProofCollection.Submissions.AutoPrTest
 import ProofCollection.Submissions.AutoIndexTest
+import ProofCollection.Submissions.FrontendTest
